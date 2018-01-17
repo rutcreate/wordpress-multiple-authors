09e2507d83f9a3dbb33d494ef36b711cc1dd74d9
Nirut Khemasakchai <rutcreate@gmail.com>
2018-01-17 11:08:45 +0700

    Validate before save.

--- includes/meta-box.php
+++ includes/meta-box.php
@@ -61,14 +61,37 @@ function multiple_authors_meta_box_save( $post_id ) {
     global $wpdb;
     if ( ! current_user_can( 'edit_post', $post_id ) ) return;
     if ( ! multiple_authors_verify_nonce( 'multiple_authors' ) ) return;
+    if ( ! isset( $_POST['multiple_authors'] ) ) return;
 
-    if ( isset( $_POST['multiple_authors'] ) ) {
-        $table_name = "{$wpdb->prefix}multiple_authors";
-        $wpdb->query("DELETE FROM {$table_name} WHERE post_id = {$post_id}");
-        foreach ( $_POST['multiple_authors'] as $section_id => $user_ids ) {
-            foreach ( $user_ids as $weight => $user_id ) {
-                $wpdb->query("INSERT INTO {$table_name} (post_id, user_id, section, weight) VALUES ({$post_id}, {$user_id}, {$section_id}, {$weight})");
+    // Section author (id:1) must be provided.
+    $authors = $_POST['multiple_authors'];
+    if ( ! is_array( $authors ) ) return;
+    if ( count( $authors ) === 0 ) return;
+    if ( ! isset( $authors[1] ) ) return;
+
+    // Author is required atleast 1 user.
+    $user_ids = $authors[1];
+    if ( ! is_array( $user_ids ) ) return;
+    if ( count( $user_ids ) === 0 ) return;
+
+    // Validate users existing.
+    foreach ( $user_ids as $user_id ) {
+        if ( ! $wpdb->get_row( "SELECT id FROM {$wpdb->prefix}users WHERE id = {$user_id}" ) ) {
+            return;
+        }
+    }
+
+    $table_name = "{$wpdb->prefix}multiple_authors";
+    $wpdb->query( "DELETE FROM {$table_name} WHERE post_id = {$post_id}" );
+    foreach ( $_POST['multiple_authors'] as $section_id => $user_ids ) {
+        $section = $wpdb->get_row( "SELECT id FROM {$wpdb->prefix}multiple_authors_section WHERE id = {$section_id}" );
+        if ( ! $section ) continue;
+
+        foreach ( $user_ids as $weight => $user_id ) {
+            if ( $section_id == 1 && $weight == 0 ) {
+                $wpdb->query( "UPDATE {$wpdb->prefix}posts SET post_author = {$user_id} WHERE ID = {$post_id}" );
             }
+            $wpdb->query( "INSERT INTO {$table_name} (post_id, user_id, section, weight) VALUES ({$post_id}, {$user_id}, {$section_id}, {$weight})" );
         }
     }
 }
